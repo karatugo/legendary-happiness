9d0659a3148a4e23d4aa92c5368fb2f95e9a6e28
karatugo <karatugo@gmail.com>
2020-03-21 15:43:07 +0300

    Introduce functional components

--- legendary-happiness/src/index.js
+++ legendary-happiness/src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+function MyApp() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <MyApp />,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
